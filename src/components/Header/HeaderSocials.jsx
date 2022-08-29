662585c56d63c638d266c21773d0ec4228b87555
Ruslan Zapata <ruslan.zapata@softtek.com>
2022-08-29 02:06:13 -0500

    Se agrego el header

---
+++ src/components/Header/HeaderSocials.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {FiDribbble} from 'react-icons/fi'
+import {BsInstagram} from 'react-icons/bs'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://linkedin.com" target="_blank" rel="noreferrer"><BsLinkedin /></a>
+        <a href="https://github.com" target="_blank" rel="noreferrer"><FaGithub /></a>
+        <a href="https://dribbble.com" target="_blank" rel="noreferrer"><FiDribbble /></a>
+        <a href="https://instagram.com" target="_blank" rel="noreferrer"><BsInstagram /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
